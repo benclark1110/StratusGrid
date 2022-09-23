e1e38a9bd6a3290c5f1456c473276bc89cbc8d4d
team-gangreen-gang <benclark1110@gmail.com>
2022-09-22 23:21:08 -0400

    Adding react-bootstrap

--- stratusgrid-app/src/App.tsx
+++ stratusgrid-app/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Pokemon App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Gotta catch 'em all!</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
