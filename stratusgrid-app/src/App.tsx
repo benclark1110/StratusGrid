f7b2d2691acbf7259ad9a5228f8604910eebc6e8
team-gangreen-gang <benclark1110@gmail.com>
2022-09-25 23:31:25 -0400

    adding modal

--- stratusgrid-app/src/App.tsx
+++ stratusgrid-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -7,8 +7,15 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -23,9 +30,37 @@ function App() {
       <Container>
         <Card>
           <Card.Body>Catch a Pokemon!
-            <Button variant="success">Catch</Button>{' '}
+            <Button variant="success" onClick={handleShow}>Catch</Button>{' '}
           </Card.Body>
         </Card>
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Pokemon Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="pokemonName">
+            <Form.Label>Pokemon Name:</Form.Label>
+            <Form.Control type="name" placeholder="Add name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="pokemonType">
+            <Form.Label>Type</Form.Label>
+            <Form.Control type="type" placeholder="Add type" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
         <br/>
         <Card>
           <Card.Body>Release a Pokemon
